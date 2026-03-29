9016ccd148e93bb9f9668df5d4627e586c4699d1
Kub1no23 <jakubzlamal235@gmail.com>
2026-03-29 18:33:57 +0200

    deploy

--- src/components/GameWindow.vue
+++ src/components/GameWindow.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref, watch, watchEffect } from 'vue'
+import { ref, watch } from 'vue'
 import { useGameMetricsStore } from '../stores/gameMetrics';
 import { usePopUpStore } from '../stores/popUp';
 import { useEndScreenStore } from '../stores/endScreen';
